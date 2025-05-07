b3f37c1f909dde921e8ed613e360d4977d91bc5b
ELIASKCOAL <154716315+ELIASKCOAL@users.noreply.github.com>
2025-05-06 21:49:54 -0400

    Update Home.tsx

--- Home.tsx
+++ Home.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, FlatList } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const HomeScreen: React.FC = ({ navigation }:any) => {  // Adicione "navigation" aqui
+const HomeScreen: React.FC = () => {  
   const [entry, setEntry] = useState('');
   const [entries, setEntries] = useState<string[]>([]);
 
@@ -17,14 +17,6 @@ const HomeScreen: React.FC = ({ navigation }:any) => {  // Adicione "navigation"
     }
   };
 
-  const addEntry = async () => {
-    if (entry.trim() === '') return;
-
-    const newEntries = [...entries, entry];
-    setEntries(newEntries);
-    setEntry('');
-    await AsyncStorage.setItem('diaryEntries', JSON.stringify(newEntries));
-  };
 
   const deleteEntry = async (index: number) => {
     const newEntries = entries.filter((_, idx) => idx !== index);
@@ -35,17 +27,7 @@ const HomeScreen: React.FC = ({ navigation }:any) => {  // Adicione "navigation"
   return (
     <View style={{ padding: 20 }}>
       <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Meu Diário</Text>
-      
-      <TextInput
-        value={entry}
-        onChangeText={setEntry}
-        placeholder="Escreva sua entrada..."
-        style={{ borderWidth: 1, padding: 10, marginVertical: 10 }}
-      />
-      
-      <Button title="Adicionar" onPress={addEntry} />
-      <Button title="Ir para Adicionar Entrada" onPress={() => navigation.navigate('AddEntry')} />  {/* Navegação */}
-      
+     
       <FlatList
         data={entries}
         keyExtractor={(item, index) => index.toString()}
